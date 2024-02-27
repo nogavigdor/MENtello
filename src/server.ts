63f6ada690c22a67cd0a9d2880dfafbaaf42d4e2
Noga Vigdor <noga.vigdor@gmail.com>
2024-02-27 01:44:25 +0100

    connected to mongoDB

--- src/server.ts
+++ src/server.ts
@@ -12,6 +12,16 @@ app.get('/api/welcome', function(req: Request, res: Response) {
     res.status(200).send({message:'Hello, Welcome to MENtello RESTful API.'});
 });
 
+mongoose.connect
+(
+    process.env.DBHOST as string,
+    { 
+        //useNewUrlParser: true,
+        //useUnifiedTopology: true  
+    }
+).catch((error):void => console.log('Error connecting to MongoDB:', error));
+
+mongoose.connection.on('connected', () : void => console.log('Connected successfully to MongoDB'));
 const PORT: number = parseInt(process.env.PORT || '4000');
 
 app.listen(PORT, function() {
